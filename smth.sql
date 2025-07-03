69b4fdfa945338df1455b809515da9bf1f971713
Andrei Codreanu <codreanu.andrei1125@gmail.com>
2025-07-03 22:22:55 +0300

    '1'

---
+++ smth.sql
@@ -0,0 +1,2 @@
+SELECT * FROM pula_mea_table 
+-- asta probabil o sa fac cu dbeaver, nu nvim ca nu imi vad conexiunile. desi s-ar putea sa fie nice. aleg intre dbeaver, pgadmin sau nvim cu un plugin de db gen dadbod 
