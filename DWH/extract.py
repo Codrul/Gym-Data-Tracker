3c321a271e9c821fb0a8317d8f7dade5a4de0d15
Andrei Codreanu <codreanu.andrei1125@gmail.com>
2025-07-07 14:04:44 +0300

    Extracted and loaded a table into Postgres with psycog2 and pandas. Now will need to add the other tables and expand on the logic. Further data modeling is incoming

--- DWH/extract.py
+++ DWH/extract.py
@@ -1,20 +1,62 @@
 import gspread
+import psycopg2
+import pandas as pd 
 from google.oauth2.service_account import Credentials
 
+# scopes here
 scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
+#credentials here
 credentials = Credentials.from_service_account_file(
     "C:\\Users\\aditz\\Gym_Data_Tracker\\APIs\\grand-strand-465118-v6-5c8c40adf654.json",
-    scopes=scopes
+    scopes= scopes
+)
+#authorize command
+gc = gspread.authorize(credentials)
+#open sheet by key
+spreadsheet = gc.open_by_key('1OiufKuY1WB_-tzfvKWZh9OHeCCEX81jQ1KHuNE5lZsQ')
+worksheet = spreadsheet.worksheet('Exercises')
+#pass records to data
+exercise_table = worksheet.get_all_records()
+
+#connecting to my db
+with open('APIs/pgpass.txt', 'r') as f:
+    db_pass = f.read().strip()
+conn = psycopg2.connect(
+    database = 'gym_data',
+    user = 'postgres',
+    password = db_pass,
+    host = 'localhost',
+    port = '5432'
 )
 
-gc = gspread.authorize(credentials)
+cursor = conn.cursor()
 
-sheet = gc.open_by_key('1OiufKuY1WB_-tzfvKWZh9OHeCCEX81jQ1KHuNE5lZsQ')
+df = pd.DataFrame(exercise_table)
+# since column names are different i will need to map them
+column_mapping = {
+    'ID':'exercise_id',
+    'Name':'exercise_name',
+    'Movement type':'exercise_movement_type',
+    'Upper/Lower':'exercise_bodysplit'
+}
+df.rename(columns= column_mapping, inplace=True)
+exercise_df = df [['exercise_id', 'exercise_name', 'exercise_movement_type',
+                   'exercise_bodysplit']]
 
-worksheet = sheet.worksheet('Exercises')
+# the insert query 
+insert_query = """ 
+    INSERT INTO staging_layer.exercises(exercise_id,exercise_name,
+    exercise_movement_type, exercise_bodysplit)
+    SELECT %s, %s, %s, %s
+    WHERE NOT EXISTS (
+        SELECT 1 FROM staging_layer.exercises WHERE exercise_id = '%s'
+    )
+"""
 
-data = worksheet.get_all_records()
-
-print(data)
-
+for row in exercise_df.itertuples(index=False):
+    cursor.execute(insert_query,(row.exercise_id, 
+                                 row.exercise_name,
+                                 row.exercise_movement_type,
+                                 row.exercise_bodysplit,
+                                 row.exercise_id))
+conn.commit()
