d3552ed88296e79640e371f83f6376a205b2cc40
Andrei Codreanu <codreanu.andrei1125@gmail.com>
2025-07-14 18:57:04 +0300

    fixed pep after linter

--- DWH/extract.py
+++ DWH/extract.py
@@ -1,6 +1,6 @@
 import gspread
 import psycopg2
-import pandas as pd 
+import pandas as pd
 from google.oauth2.service_account import Credentials
 
 # scopes here
@@ -8,25 +8,25 @@ scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 #credentials here
 credentials = Credentials.from_service_account_file(
     "C:\\Users\\aditz\\Gym_Data_Tracker\\APIs\\grand-strand-465118-v6-5c8c40adf654.json",
-    scopes= scopes
+    scopes=scopes
 )
-#authorize command
+# authorize command
 gc = gspread.authorize(credentials)
-#open sheet by key
+# open sheet by key
 spreadsheet = gc.open_by_key('1OiufKuY1WB_-tzfvKWZh9OHeCCEX81jQ1KHuNE5lZsQ')
 worksheet = spreadsheet.worksheet('Exercises')
-#pass records to data
+# pass records to data
 exercise_table = worksheet.get_all_records()
 
-#connecting to my db
+# connecting to my db
 with open('APIs/pgpass.txt', 'r') as f:
     db_pass = f.read().strip()
 conn = psycopg2.connect(
-    database = 'gym_data',
-    user = 'postgres',
-    password = db_pass,
-    host = 'localhost',
-    port = '5432'
+    database='gym_data',
+    user='postgres',
+    password=db_pass,
+    host='localhost',
+    port='5432'
 )
 
 cursor = conn.cursor()
@@ -34,17 +34,17 @@ cursor = conn.cursor()
 df = pd.DataFrame(exercise_table)
 # since column names are different i will need to map them
 column_mapping = {
-    'ID':'exercise_id',
-    'Name':'exercise_name',
-    'Movement type':'exercise_movement_type',
-    'Upper/Lower':'exercise_bodysplit'
+    'ID': 'exercise_id',
+    'Name': 'exercise_name',
+    'Movement type': 'exercise_movement_type',
+    'Upper/Lower': 'exercise_bodysplit'
 }
-df.rename(columns= column_mapping, inplace=True)
-exercise_df = df [['exercise_id', 'exercise_name', 'exercise_movement_type',
+df.rename(columns=column_mapping, inplace=True)
+exercise_df = df[['exercise_id', 'exercise_name', 'exercise_movement_type',
                    'exercise_bodysplit']]
 
-# the insert query 
-insert_query = """ 
+# the insert query
+insert_query = """
     INSERT INTO staging_layer.exercises(exercise_id,exercise_name,
     exercise_movement_type, exercise_bodysplit)
     SELECT %s, %s, %s, %s
@@ -54,10 +54,10 @@ insert_query = """
 """
 
 for row in exercise_df.itertuples(index=False):
-    cursor.execute(insert_query,(row.exercise_id, 
+    cursor.execute(insert_query,(row.exercise_id,
                                  row.exercise_name,
                                  row.exercise_movement_type,
                                  row.exercise_bodysplit,
                                  row.exercise_id))
 conn.commit()
-# adding a comment to test linter 
+# adding a comment to test linter
