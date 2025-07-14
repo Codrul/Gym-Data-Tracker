ac069b56f03c5de3f50ed2d370e90a9c44787701
Andrei Codreanu <codreanu.andrei1125@gmail.com>
2025-07-14 23:19:08 +0300

    worked on the ingestion pipeline and created the basic functions. forgot to branch this out but thankfully at this stage of the project it's not like I'm breaking prod haha amirite...

--- DWH/extract.py
+++ DWH/extract.py
@@ -43,6 +43,22 @@ exercise_muscle = Table(
     schema='staging_layer'
 )
 
+muscles = Table(
+    'muscles', metadata,
+    Column('muscle_id', String, primary_key=True),
+    Column('muscle_name', String),
+    Column('muscle_group', String),
+    schema='staging_layer'
+)
+
+resistance_types = Table(
+    'resistance_types', metadata,
+    Column('resistance_id', String, primary_key=True),
+    Column('resistance_type', String),
+    Column('resistance_category', String),
+    schema='staging_layer'
+)
+
 
 def load_exercises():
     try:
@@ -149,6 +165,102 @@ def load_exercise_muscle():
         print(f'Error {e} occurred. Could not insert into exercise_muscle')
 
 
-# Run functions
+def load_muscles():
+    try:
+        spreadsheet = gc.open_by_key(
+            '1OiufKuY1WB_-tzfvKWZh9OHeCCEX81jQ1KHuNE5lZsQ'
+        )
+        worksheet = spreadsheet.worksheet('Muscles')
+        muscles_table = worksheet.get_all_records()
+    except Exception as e:
+        print(f'Error {e} occurred. Failed to load from Google Sheets')
+        return
+
+    df = pd.DataFrame(muscles_table)
+    column_mapping = {
+        'ID': 'muscle_id',
+        'Muscle name': 'muscle_name',
+        'Muscle groups': 'muscle_group',
+    }
+    df.rename(columns=column_mapping, inplace=True)
+
+    df = df[
+        ['muscle_id', 'muscle_name', 'muscle_group']
+    ]
+
+    try:
+        inserted_rows = 0
+        with engine.connect() as conn:
+            for row in df.itertuples(index=False):
+                exists_stmt = select(muscles.c.muscle_id).where(
+                    (muscles.c.muscle_id == str(row.muscle_id)))
+                result = conn.execute(exists_stmt).fetchone()
+                if result is None:
+                    ins_stmt = insert(muscles).values(
+                        muscle_id=row.muscle_id,
+                        muscle_name=row.muscle_name,
+                        muscle_group=row.muscle_group,
+                    )
+                    conn.execute(ins_stmt)
+                    inserted_rows += 1
+            conn.commit()
+        return (
+            f"{inserted_rows} rows have been loaded "
+            "into *staging_layer.muscle*"
+        )
+    except Exception as e:
+        print(f'Error {e} occurred. Could not insert into muscle')
+
+
+def load_resistance_types():
+    try:
+        spreadsheet = gc.open_by_key(
+            '1OiufKuY1WB_-tzfvKWZh9OHeCCEX81jQ1KHuNE5lZsQ'
+        )
+        worksheet = spreadsheet.worksheet('Resistance_types')
+        resistance_table = worksheet.get_all_records()
+    except Exception as e:
+        print(f'Error {e} occurred. Failed to load from Google Sheets')
+        return
+
+    df = pd.DataFrame(resistance_table)
+    column_mapping = {
+        'Resistance_ID': 'resistance_id',
+        'Resistance': 'resistance_type',
+        'Resistance_category': 'resistance_category',
+    }
+    df.rename(columns=column_mapping, inplace=True)
+
+    df = df[
+        ['resistance_id', 'resistance_type', 'resistance_category']
+    ]
+
+    try:
+        inserted_rows = 0
+        with engine.connect() as conn:
+            for row in df.itertuples(index=False):
+                exists_stmt = select(resistance_types.c.resistance_id).where(
+                    (resistance_types.c.resistance_id == str(row.resistance_id)))
+                result = conn.execute(exists_stmt).fetchone()
+                if result is None:
+                    ins_stmt = insert(resistance_types).values(
+                        resistance_id=row.resistance_id,
+                        resistance_type=row.resistance_type,
+                        resistance_category=row.resistance_category,
+                    )
+                    conn.execute(ins_stmt)
+                    inserted_rows += 1
+            conn.commit()
+        return (
+            f"{inserted_rows} rows have been loaded "
+            "into *staging_layer.resistance_types*"
+        )
+    except Exception as e:
+        print(f'Error {e} occurred. Could not insert into muscle')
+
+
+# don't forget to run these lol
 print(load_exercises())
 print(load_exercise_muscle())
+print(load_muscles())
+print(load_resistance_types())
