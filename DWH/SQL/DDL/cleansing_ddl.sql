11d60ca35a8d8aca3acd46acad5da036b45f9b6d
Andrei Codreanu <codreanu.andrei1125@gmail.com>
2025-07-17 22:56:50 +0300

    pushing to master directly. added cleansing layer and fact table to dwh

--- DWH/SQL/DDL/cleansing_ddl.sql
+++ DWH/SQL/DDL/cleansing_ddl.sql
@@ -1,34 +1,33 @@
-CREATE SCHEMA IF NOT EXISTS staging_layer;
+CREATE SCHEMA IF NOT EXISTS cleansing_layer; 
 
-CREATE TABLE IF NOT EXISTS staging_layer.exercises(
+CREATE TABLE IF NOT EXISTS cl_exercises(
 	exercise_id VARCHAR(256),
 	exercise_name VARCHAR(256),
 	exercise_movement_type VARCHAR(256),
 	exercise_bodysplit VARCHAR(256)
 );
 
-CREATE TABLE IF NOT EXISTS staging_layer.muscles(
+CREATE TABLE IF NOT EXISTS cl_muscles(
 	muscle_id VARCHAR(256),
 	muscle_name VARCHAR(256),
 	muscle_group VARCHAR(256)
 );
 
-CREATE TABLE IF NOT EXISTS staging_layer.resistance_types(
+CREATE TABLE IF NOT EXISTS cl_resistance_types(
 	resistance_id VARCHAR(256),
 	resistance_type VARCHAR(256),
 	resistance_category VARCHAR(256)
 );
 
-CREATE TABLE IF NOT EXISTS staging_layer.exercise_muscle (
+CREATE TABLE IF NOT EXISTS cl_exercise_muscle(
 	exercise_id VARCHAR(256),
 	exercise_name VARCHAR(256),
 	muscle_id VARCHAR(256),
 	muscle_name VARCHAR(256),
 	muscle_role VARCHAR(256)
 );
- -- will need to create the fact table after I think it through 
 
-CREATE TABLE IF NOT EXISTS staging_layer.workouts(
+CREATE TABLE IF NOT EXISTS cl_workouts(
 	workout_id VARCHAR(256),
 	"date" VARCHAR(256),
 	set_number VARCHAR(256),
@@ -37,7 +36,6 @@ CREATE TABLE IF NOT EXISTS staging_layer.workouts(
 	"load" VARCHAR(256),
 	resistance_type VARCHAR(256),
 	set_type VARCHAR(256),
-	"comments" VARCHAR(256),
+	"comments" VARCHAR(256), 
 	workout_type VARCHAR(256)
-
 );
